30f7441cedf88a464b30244666133f1c4318a25a
itonse <govl3dnjs@gmail.com>
2023-11-13 15:36:03 +0900

    feat : 2-08 ROOT URL
    - redirect, forward

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -12,4 +12,10 @@ public class MainController {
     public String index() {
         return "안녕하세요 sbb에 오신것을 환영합니다.";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";     // URL로 리다이렉트 (완전히 새로운 URL로 요청)
+                                              // forward: - URL로 포워드 (기존 요청 값들이 유지된 상태로 URL이 전환)
+    }
 }
