a0cbe9f604f5a5ae8f57ac7535f3d1a81e6887e5
itonse <govl3dnjs@gmail.com>
2023-11-13 14:25:31 +0900

    feat : 2-05 리포지터리 까지 완료

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)   // 질문을 삭제하면 그에 달린 답변들도 모두 함께 삭제
+    private List<Answer> answerList;
+}
